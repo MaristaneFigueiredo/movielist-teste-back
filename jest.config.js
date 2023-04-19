e9ad8c2a2514cd1641022b2a4ea5f77f40808535
Maristane Figueiredo <maristanetecnologia@gmail.com>
2023-04-19 17:36:23 -0300

    refact: configuration jest.config

--- jest.config.js
+++ jest.config.js
@@ -8,37 +8,16 @@
     testMatch: ["<rootDir>/tests/*.(test|spec).ts"]
 } */
 
- 
-/* export default {
-    preset: "ts-jest", // conjunto de configurações que será executada
-    testEnvironment:"node", // qual o ambiente será rodado os testes
-  //  moduleDirectories: ["node-modules", "src"],
-     moduleDirectories: ["<rootDir>/node_modules", "<rootDir>/src"],
-    transform: {
-       ".+\\.ts$": "ts-jest", // forma de tranformação dos arquivos ts, usando ts-jest
-   //    "^.+\\.(js|ts)$": "ts-jest",
-    }, 
-    testMatch: ["<rootDir>/tests/*.(test|spec).ts"], // Local onde estará os nossos arquivos de teste e sintaxe de como eles estarão escritos.
-    //setupFilesAfterEnv: ['source-map-support/register'],
-    moduleFileExtensions: ['ts', 'js'],
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1', // substitua pelo caminho correto para o diretório do seu projeto
-      },
-}  */
-
 //obs.: projetos diferentes, pode ter configurações diferentes
-export default  {
-    preset: "ts-jest",
-    testEnvironment:"node",
-    moduleDirectories: ["<rootDir>/node_modules", "<rootDir>/src"],
-    transform: {
-        ".+\\.ts$": "ts-jest",
-    }, 
-    testMatch: ["<rootDir>/tests/*.(test|spec).ts"],
+export default {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleDirectories: ["<rootDir>/node_modules", "<rootDir>/src"],
+  transform: {
+    ".+\\.ts$": "ts-jest",
+  },
+  testMatch: ["<rootDir>/tests/*.(test|spec).ts"],
   /*   server:{
         port:5001,
     } */
-    
-
-
 }
--- tests/movies.test.ts
+++ tests/movies.test.ts
@@ -33,42 +33,6 @@ describe("Testanto as Rotas de Movies", () => {
   })
 })
 
-/*obs.:No Jest, o describe é uma FUNÇÃO de agrupamento usada para organizar e categorizar casos de teste em BLOCOS. 
-Ele é usado para criar blocos de testes relacionados, permitindo que você forneça uma descrição e agrupe vários casos de teste sob esse contexto
-Os descritores devem ser usados APENAS para fornecer uma DESCRIÇÃO do contexto dos casos de teste
-Sintaxe:    
-    describe('Testes de exemplo', () => {
-        // Casos de teste aqui
-    });
-test ou it: É usado para escrever casos de teste individuais dentro de um bloco "describe"
-expect: é usada para verificar se um valor atende a uma determinada condição
-        expect(valor).toBe(3); // Verifica se o valor é igual a 3
-toBe: É uma função de asserção que verifica se dois valores são estritamente iguais em valor e tipo. 
-toEqual: É uma função de asserção que verifica se dois OBJETOS ou ARRAYS são iguais em termos de propriedades/elementos e seus respectivos valores.
-        expect(objeto).toEqual({ chave: 'valor' }); // Verifica se o objeto possui a chave com o valor esperado
-
-        expect (result.body).toEqual({
-            id: expect.any(Number), // Espero QUALQUER VALOR desde que seja numero
-            title: expect.any(String) //Espero QUALQUER VALOR desde que seja string
-        })
-toBeTruthy e toBeFalsy: São funções de asserção que verificam se um valor é verdadeiro ou falso, respectivamente
-toBeGreaterThan e toBeLessThan: São funções de asserção que verificam se um valor é maior ou menor do que um valor específico, respectivamente
-toContain: É uma função de asserção que verifica se um valor específico está presente em um array ou em uma string
-toThrow e toThrowError: São funções de asserção que verificam se uma função lança uma exceção ou erro quando é chamada.
-            expect(() => minhaFuncao()).toThrow(); // Verifica se minhaFuncao() lança uma exceção
-            expect(() => minhaFuncao()).toThrowError('Mensagem de erro'); // Verifica se minhaFuncao() lança um erro com uma determinada mensagem
-
-not: É um modificador que pode ser usado em conjunto com outras funções de asserção para verificar a negação de uma condição.
-    expect(valor).not.toBe(3); // Verifica se o valor não é igual a 3
-toHaveLength: comparar tamanho. Ex.: tamanho de um array
-arrayContaining: Qdo desejo um subjconjunto de um array, ou seja, um recorte de um array principal
-        Ex.:  expect(response.body).toEqual(expect.arrayContaining([])) 
-toMatchObject: Quando considero partes de um objeto
-        Ex.:  expect(response.body).toMatchObject({"title": "Developer III"})
-                expect(objetoTestado).toMatchObject(objetoEsperado);
-
-         É importante notar que toMatchObject faz uma comparação parcial dos objetos, ou seja, não é necessário que todos os pares de propriedade-valor do objetoEsperado estejam presentes no objetoTestado. Propriedades adicionais no objetoTestado não serão consideradas na comparação.
-*/
 /* instalar dotenv-cli
         npm i -D dotenv-cli
 */
